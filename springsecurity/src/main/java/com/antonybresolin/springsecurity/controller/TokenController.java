36569133a4fdaa1c442b092a6a7b11f53484e18a
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-07-07 23:08:48 -0300

    ADMIN - Listar todos usuarios
    obs: correção no insert de roles inicial

--- springsecurity/src/main/java/com/antonybresolin/springsecurity/controller/TokenController.java
+++ springsecurity/src/main/java/com/antonybresolin/springsecurity/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.antonybresolin.springsecurity.controller;
 
 import com.antonybresolin.springsecurity.controller.dto.LoginRequest;
 import com.antonybresolin.springsecurity.controller.dto.LoginResponse;
+import com.antonybresolin.springsecurity.entities.Role;
 import com.antonybresolin.springsecurity.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -38,11 +40,18 @@ public class TokenController {
         var now = Instant.now();
         var expiration = 3600L; // 1 hora
 
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
+
         var claims = JwtClaimsSet.builder()
                 .issuer("backend")
                 .subject(user.get().getUsername())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiration))
+                .claim("scope", scopes)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
